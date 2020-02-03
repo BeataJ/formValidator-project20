c73345faac956cd47abc5efabbdcbb35ec4e166c
beata <beata@emc2wave.com>
2020-02-03 14:32:56 -0800

    add to checkEmail()  if statmennts

--- script.js
+++ script.js
@@ -21,7 +21,11 @@ function showSuccess(input) {
 // valided email
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input)) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 // Check required fields
