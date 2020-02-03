2a82fd6da9c0d862ab580c120e5dbaaef8130c48
beata <beata@emc2wave.com>
2020-02-03 14:29:35 -0800

    change name to checkEmail()

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function showSuccess(input) {
 }
 
 // valided email
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
