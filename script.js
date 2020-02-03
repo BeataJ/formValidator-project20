0275c0df63cb7edb1a0e462edec7d90e9da3a88f
beata <beata@emc2wave.com>
2020-02-02 21:07:08 -0800

    create showError()

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show input error
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Event Listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
